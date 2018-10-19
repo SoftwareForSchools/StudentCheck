5c6c17b07e48ac675393118ee5ce257bbf0c90d5
Michel <michelrummens@hotmail.com>
2018-10-19 10:57:43 +0200

    WV: Added connecting to database from within ui. Added DatabaseConnection Singleton.

---
+++ android/SymposiumCheckIn/app/src/main/java/com/hva/symposiumcheckin/database/DatabaseConnection.java
@@ -0,0 +1,82 @@
+package com.hva.symposiumcheckin.database;
+
+import com.hva.symposiumcheckin.R;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.MessageFormat;
+
+public class DatabaseConnection {
+
+    private static Connection connection;
+    private static DatabaseConnection singleton;
+    private static boolean status;
+
+    // TODO: Create a 'remember me' button.
+    private String dbName;
+    private String username;
+    private String password;
+
+    static {
+        singleton = new DatabaseConnection();
+    }
+
+    private DatabaseConnection() {
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://oege.ie.hva.nl:3306/"+ dbName+"?useUnicode=true&useJDBCCompliantTimezoneShift" +
+                        "=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
+                status = true;
+            } catch (Exception e) {
+                status = false;
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() {
+        return singleton;
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean setUserInfo(String dbName, String username, String password){
+        try{
+            this.dbName = dbName;
+            this.username = username;
+            this.password = password;
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+}
